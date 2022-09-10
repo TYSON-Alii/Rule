8d49c1a7791fed1e06e9eaf96039e188b21ecbc5
Muhammed Ali <65432197+TYSON-Alii@users.noreply.github.com>
2022-09-10 18:09:43 +0300

    Update __cxx_utility.hxx

--- __cxx_utility.hxx
+++ __cxx_utility.hxx
@@ -31,13 +31,18 @@ inline auto operator echo(auto v) { return cout << v << '\n'; }
 $def print[...]{ cout << [... << ' ' << ] }
 $def for[it_name:list]{ for (auto it_name = list.begin(); it_name != list.end(); it_name++) }
 $def list.map(list : find => make) { (for (auto& i : list) if i == find { i = make; }) }
-$def if<_if ? _con : _else> { ([&]() { if _con{ return _if; } return _else; }()) }
-$def range[start .. end, step]{
-	([&]()->auto {
-		vector<decltype(start)> v;
-		for (auto i = start; i != end; i += step) v.push_back(i);
-		return v;
-	}())
+inline const if_else(bool con, auto _if, auto _else) {
+	if _con { return _if; } return _else;
+}
+$def if<_if ? _con : _else> { if_else(_con, _if, _else) }
+$def slice[list, beg .. end] { decltype(list)(list.begin()+beg, list.begin() + end) }
+inline auto range_fn(start, end, step) {
+	vector<decltype(start)> v;
+	for (auto i = start; i < end; i += step) v.push_back(i);
+	return v;
+}
+$def range[start ..end, step] {
+	range_fn(start, end, step)
 }
 $def once { { static const auto Once = [&]() { __arg__ return nullptr; }(); } }
 $macro self (*this)
@@ -69,3 +74,4 @@ $macro f32 float
 $macro f64 double
 $macro list std::pmr::vector
 $macro ptr auto*
+$nspace std
