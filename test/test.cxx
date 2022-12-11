b0534f52653018090f5cf4a2a802de890a9e7edf
Muhammed Ali <65432197+TYSON-Alii@users.noreply.github.com>
2022-12-11 13:03:30 +0300

    Update test.cxx

--- test/test.cxx
+++ test/test.cxx
@@ -29,18 +29,26 @@ $def log'arg' { // that's signle line, log` ` support multi line
 const func() -> bool {
 	return true;
 }
+$sep in
+$def For[i in l] {
+	for (auto&& i : l)
+}
+#include "Game.hpp" as xs
 const main() -> int {
 	std.size_t i = 0;
 	if<"selm" ? true or false : "mrb">;
 	// comment
 	list<int> l{ 1,1,1,5,6,1,8 };
 	list.map(l : 1 => 31);
+	For[i in l] {
+		echo i;
+	}
 	print[1, selam, "meaba"];
 	average(1, 2, 3, 4, 5);
 	r_list := range[2 .. 5, 1];
 	&ref_list := r_list;
 	[r,g,b] := my_color;
-	for (let i : @[1,4,1,5,7])
+	for (const& i : @[1,4,1,5,7])
 		echo i;
 	err<"error" : "check this after", "line:", 5>;
 	str hello = "Hello";
@@ -50,15 +58,17 @@ const main() -> int {
 	std::cout << f"{hello+f`wow {math.pi}.`}, World." << '\n';
 	str falanke = `C:\wow\amazing`;
 	int beg = 10, end = 21;
-	for (const&& i : beg..end) {
+	for(let i : beg..end) {
 		once{
 			echo f"-_- {i}";
 		}
-		falan filan(i + 1);
+		falan filan (i + 1);
 	}
 	if 2 + 2 == 4 { // require curly brackets
 		echo "evet.";
 	}
+	if (2 + 2 == 5):
+		echo "hayir";
 	/* amazing comment */
 	fn func(int wow) {
 		if true or false {
@@ -77,6 +87,7 @@ const main() -> int {
 	const func = [&](str) { /* falan filan */ };
 	func(""s);
 	$rep[31:30 + math.pi] func(__n__);
+	math.pi;
 	return 0;
 };
 int operator filan(int v) {
